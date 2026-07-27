e683a6247c1df542b60bb94921f162d3dcbd9475
Raunak Kumar <raunak_cse25@delhitechnicalcampus.ac.in>
2026-07-27 17:56:33 +0000

    oneJS

---
+++ 04_control_flow/one.js
@@ -0,0 +1,52 @@
+//if 
+const isUserloggedIn = true
+if(2 === "2"){
+    // console.log("Executed");
+}
+
+const temperature = 42
+if(temperature <50){
+    // console.log("Less than 50");
+}
+else{
+
+    // console.log("Temperature is greater tham 50")
+}
+
+const score = 200
+
+if(score > 100){
+    const power = "FLY"
+    // console.log(`User Power: ${power}`);
+}
+
+const balance = 1000
+// if(balance > 500) console.log("Test");
+
+// if (balance < 500){
+//     console.log("Less than");
+// } else if(balance<750){
+//     console.log("Less than 750");
+// }
+//  else if(balance<900){
+//     console.log("Less than 900");
+// }
+//  else{
+//     console.log("Less than 1200");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggesInFromGoogle = false
+const loggesInFromEmail = true
+if(userLoggedIn && debitCard){
+    // console.log("Allow to buy course");
+
+}
+
+
+if( loggesInFromGoogle || loggesInFromEmail){
+    console.log("User logged in");
+
+}
