b67c3744f2ce6d5e30623795b861f6e064bc67e3
Raunak Kumar <raunak_cse25@delhitechnicalcampus.ac.in>
2026-07-28 02:48:21 +0000

    fourJS

---
+++ 05_iterations/four.js
@@ -0,0 +1,32 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+// for (const key in myObject) {
+//     console.log(key);     
+// }
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);     
+}
+
+
+const prigramming = ["Js", "C++", "Java", "rb"]
+
+for (const key in prigramming) {
+    console.log(prigramming[key])
+}
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('JP', "Japan")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
