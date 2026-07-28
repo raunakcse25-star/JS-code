30315a77ae074dbfe87ba259b4d8fe5efc4389af
Raunak Kumar <raunak_cse25@delhitechnicalcampus.ac.in>
2026-07-28 02:47:53 +0000

    TwoJS

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+
+
+let i = 0
+while (i <= 10) {
+    // console.log(`value of i is ${i}`);
+    i = i + 2
+}
+let myArray = ["Flas ", "Batman", "Superman"]
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`)
+    arr = arr + 1
+}
+
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
