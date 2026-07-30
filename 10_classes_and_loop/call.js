670df6d7fdbd8e4653a4aa416bb38567d8198046
Raunak Kumar <raunak_cse25@delhitechnicalcampus.ac.in>
2026-07-30 12:12:02 +0000

    calls

---
+++ 10_classes_and_loop/call.js
@@ -0,0 +1,13 @@
+function SetUsername(username){
+    //complex DB calls
+    this.username = username
+    console.log("Called");
+}
+function createUser(username,email,password){
+    SetUsername.call(this,username)
+    
+    this.email = email
+    this.password = password
+}
+const chai = new createUser("raunak","raunak@gmail.com",123)
+console.log(chai);
